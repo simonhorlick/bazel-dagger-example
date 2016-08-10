18d0c20ce61db9f48cf1af907f363d11cb783828
Simon Horlick <simonhorlick@gmail.com>
2016-08-10 10:09:10 +0800

    Add unit test

---
+++ javatests/coffee/CoffeeMakerTest.java
@@ -0,0 +1,11 @@
+package coffee;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CoffeeMakerTest {
+
+  @Test public void testHeaterIsTurnedOnAndThenOff() {
+    fail("Not implemented");
+  }
+}
